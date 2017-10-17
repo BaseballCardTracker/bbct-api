79fff88940d074c21d275a10ad3c7636eaa02e7d
Code Apprentice <codeguru42@gmail.com>
2017-10-17 06:55:17 -0600

    Create models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class BrandName(models.Model):
+    band_name = models.CharField(max_length=25)
+
+
+class PlayerName(models.Model):
+    player_name = models.CharField(max_length=25)
+
+
+class TeamName(models.Model):
+    team_name = models.CharField(max_length=25)
