c75fcc2c58864008dceef90badb1a8779a5215e5
Code Apprentice <codeguru42@gmail.com>
2017-10-17 07:09:04 -0600

    Register models with admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import BrandName, PlayerName, TeamName
+
+admin.site.register(BrandName)
+admin.site.register(PlayerName)
+admin.site.register(TeamName)
