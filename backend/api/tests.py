96ec33a4974169957e317bfe66bc7e5d3bc499d5
Code Apprentice <codeguru42@gmail.com>
2017-10-20 19:56:01 -0600

    Implement test_get_brand_name()

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,12 +1,19 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 
+from api.models import BrandName
+
 
 class BrandNameTests(TestCase):
     def setUp(self):
+        self.url = reverse('brand-names')
         self.client = Client()
 
     def test_endpoint_exists(self):
-        url = reverse('brand-names')
-        response = self.client.get(url)
+        response = self.client.get(self.url)
         self.assertEqual(200, response.status_code)
+
+    def test_get_brand_name(self):
+        brand_name = BrandName.objects.create(brand_name='Topps')
+        response = self.client.get(self.url)
+        self.assertEqual(brand_name, response.json())
