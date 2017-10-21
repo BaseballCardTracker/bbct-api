f09b8e5e4bddf63449301b2a956b629756039b40
Code Apprentice <codeguru42@gmail.com>
2017-10-21 16:45:40 -0600

    Fix name of test: use plural

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,4 +1,5 @@
 import json
+
 from django.test import TestCase, Client
 from django.urls import reverse
 
@@ -14,7 +15,7 @@ class BrandNameTests(TestCase):
         response = self.client.get(self.url)
         self.assertEqual(200, response.status_code)
 
-    def test_get_brand_name(self):
+    def test_get_brand_names(self):
         brand_name = BrandName.objects.create(brand_name='Topps')
         response = self.client.get(self.url)
         result = json.loads(response.json())
