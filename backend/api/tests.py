122173a67bcf40b71e4a3838e69494d9997a6caf
Code Apprentice <codeguru42@gmail.com>
2017-10-21 16:55:46 -0600

    Implement PlayerNameTests

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -3,7 +3,7 @@ import json
 from django.test import TestCase, Client
 from django.urls import reverse
 
-from api.models import BrandName
+from api.models import BrandName, PlayerName
 
 
 class BrandNameTests(TestCase):
@@ -20,4 +20,21 @@ class BrandNameTests(TestCase):
         response = self.client.get(self.url)
         result = json.loads(response.json())
         expected = {'brand_name': brand_name.brand_name}
+        self.assertEqual(expected, result[0]['fields'])
+
+
+class PlayerNameTests(TestCase):
+    def setUp(self):
+        self.url = reverse('player-names')
+        self.client = Client()
+
+    def test_endpoint_exists(self):
+        response = self.client.get(self.url)
+        self.assertEqual(200, response.status_code)
+
+    def test_get_player_names(self):
+        player_name = PlayerName.objects.create(player_name='Alex Fernandez')
+        response = self.client.get(self.url)
+        result = json.loads(response.json())
+        expected = {'player_name': player_name.player_name}
         self.assertEqual(expected, result[0]['fields'])
