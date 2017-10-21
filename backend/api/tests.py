66dbb9baebab4a827e1bfd2237d1a99ff618c6ba
Code Apprentice <codeguru42@gmail.com>
2017-10-20 19:28:55 -0600

    Rename test class and method

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,11 +1,11 @@
 from django.test import TestCase, Client
 
 
-class EndPointTests(TestCase):
+class BrandNameTests(TestCase):
     def setUp(self):
         self.client = Client()
 
-    def test_brand_names(self):
+    def test_endpoint_exists(self):
         url = '/api/brand_names/'
         response = self.client.get(url)
         self.assertEqual(200, response.status_code)
