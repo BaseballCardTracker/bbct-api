10ceaa5d7974e327bf628a1d8855347dfe69760b
Code Apprentice <codeguru42@gmail.com>
2017-10-20 19:38:23 -0600

    Add names to all url patterns to ease reverse lookup

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from api import views
 
 urlpatterns = [
-    url(r'^brand_names/', views.get_brand_names),
-    url(r'^player_names/', views.get_player_names),
-    url(r'^team_names/', views.get_team_names),
-    url(r'^positions/', views.get_positions),
+    url(r'^brand_names/', views.get_brand_names, name='brand-names'),
+    url(r'^player_names/', views.get_player_names, name='player-names'),
+    url(r'^team_names/', views.get_team_names, name='team-names'),
+    url(r'^positions/', views.get_positions, name='positions'),
 ]
