493f6aa553b34f781eff6cb6926820190965a52c
Code Apprentice <codeguru42@gmail.com>
2017-10-22 10:04:38 -0600

    Register Position model with admin site

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import BrandName, PlayerName, TeamName
+from .models import BrandName, PlayerName, TeamName, Position
 
 admin.site.register(BrandName)
 admin.site.register(PlayerName)
 admin.site.register(TeamName)
+admin.site.register(Position)
