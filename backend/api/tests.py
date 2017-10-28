9ebbb44f7822fd1131777c0c93d1b14c1d43401e
Code Apprentice <codeguru42@gmail.com>
2017-10-27 20:39:45 -0600

    Add more test data to prepare for query tests

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -8,43 +8,54 @@ from api.models import BrandName, PlayerName, TeamName, Position
 
 class BbctTests:
     def test_get(self):
-        self.model_class.objects.create(**self.fields)
+        for field in self.fields:
+            self.model_class.objects.create(**field)
         response = self.client.get(self.url)
         self.assertEqual(200, response.status_code)
         result = json.loads(response.json())
-        self.assertEqual(self.fields, result[0]['fields'])
+
+        for expected, actual in zip(self.fields, result):
+            self.assertEqual(expected, actual['fields'])
 
     def test_post(self):
-        fields_json = json.dumps(self.fields)
+        fields_json = json.dumps(self.fields[0])
         response = self.client.post(self.url, data=fields_json, content_type='application/json')
         self.assertEqual(200, response.status_code)
         data = json.loads(response.json())
-        self.assertEqual(self.fields, data[0]['fields'])
+        self.assertEqual(self.fields[0], data[0]['fields'])
 
 
 class BrandNameTests(TestCase, BbctTests):
     def setUp(self):
         self.model_class = BrandName
-        self.fields = {'brand_name': 'Topps'}
+        self.field_name = 'brand_name'
+        self.fields = [{self.field_name: 'Topps'},
+                       {self.field_name: 'Upper Deck'}]
         self.url = reverse('brand-names')
 
 
 class PlayerNameTests(TestCase, BbctTests):
     def setUp(self):
         self.model_class = PlayerName
-        self.fields = {'player_name': 'Alex Fernandez'}
+        self.field_name = 'player_name'
+        self.fields = [{self.field_name: 'Alex Fernandez'},
+                       {self.field_name: 'Dave Hollins'}]
         self.url = reverse('player-names')
 
 
 class TeamNameTests(TestCase, BbctTests):
     def setUp(self):
         self.model_class = TeamName
-        self.fields = {'team_name': 'White Sox'}
+        self.field_name = 'team_name'
+        self.fields = [{self.field_name: 'White Sox'},
+                       {self.field_name: 'Phillies'}]
         self.url = reverse('team-names')
 
 
 class PositionTests(TestCase, BbctTests):
     def setUp(self):
         self.model_class = Position
-        self.fields = {'position': 'Pitcher'}
+        self.field_name = 'position'
+        self.fields = [{self.field_name: 'Pitcher'},
+                       {self.field_name: 'Third Base'}]
         self.url = reverse('positions')
