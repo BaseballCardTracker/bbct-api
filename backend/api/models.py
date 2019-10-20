a9244c5c102c5ae72beb60badc875472185cd3e3
Code-Apprentice <codeguru42@gmail.com>
2019-10-19 19:08:12 -0600

    BaseballCard model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseballCard(models.Model):
+    autographed = models.BooleanField(default=False)
+    condition = models.CharField(max_length=32)
+    brand = models.CharField(max_length=32)
+    year = models.IntegerField()
+    number = models.CharField(max_length=32)
+    value = models.DecimalField(null=True, max_digits=20, decimal_places=2)
+    quantity = models.IntegerField()
+    player = models.CharField(max_length=32)
+    team = models.CharField(max_length=32)
+    position = models.CharField(max_length=32)
