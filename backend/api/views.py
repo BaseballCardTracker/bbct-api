217c830ba1a224e6106628d4ad20ec4e9649ac6a
Code-Apprentice <codeguru42@gmail.com>
2019-10-19 19:13:31 -0600

    Add a viewset

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from api import models, serializers
+
+
+class BaseballCardViewSet(viewsets.ModelViewSet):
+    queryset = models.BaseballCard.objects.all()
+    serializer_class = serializers.BaseballCardSerializer
