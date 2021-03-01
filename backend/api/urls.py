834911fd1eb891584ef98cc19a017ea7829eb4ab
Code-Apprentice <codeguru42@gmail.com>
2021-02-28 23:42:31 -0700

    Generate dynamic openapi schema

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework.schemas import get_schema_view
 
 from api import views
 
@@ -8,6 +9,13 @@ app_name = 'api'
 router = routers.DefaultRouter()
 router.register(r'baseball_cards', views.BaseballCardViewSet)
 
+schema_view = get_schema_view(
+    title="BBCT",
+    description="Baseball Card Tracker API",
+    version="1.0.0"
+)
+
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('docs/', schema_view, name='docs'),
 ]
