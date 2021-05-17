7706fbbb48060710f9694fb6cc26a8588f8c8d80
Code-Apprentice <codeguru42@gmail.com>
2021-05-16 19:15:31 -0600

    Add DEFAULT_AUTO_FIELD to settings
    This is new in Django 3.2

--- backend/bbct/settings.py
+++ backend/bbct/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
