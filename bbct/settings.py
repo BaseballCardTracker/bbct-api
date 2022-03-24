3076f80ec30a7299512ca03e84bf60a4f2ac09ce
Code-Apprentice <codeguru42@gmail.com>
2022-03-23 19:53:38 -0600

    Read .env file

--- bbct/settings.py
+++ bbct/settings.py
@@ -14,6 +14,9 @@ import os
 from envparse import env
 
 
+env.read_envfile()
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
